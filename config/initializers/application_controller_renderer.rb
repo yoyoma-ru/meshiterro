3d28aaf0f946ea7e8964f85b63e3a7f10b61c2cb
愿山聖 <asjstksmr@gmail.com>
2020-05-31 20:27:58 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '280f3a6a39acf44015adeddc4455b0007ca9eecd3f67cfb2169e11c929cc5b08f8f06ae5f3d990a5febb15b8f7ad9b0c4092e52f64bc0b166d186b4a2527d77c'
